9fe7e752f98f4b753d52917d66a9a5b9272d4d45
amanagarwal05 <52529517+amanagarwal05@users.noreply.github.com>
2021-10-28 15:26:56 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,9 +1,10 @@
 import abc1
-import os
+#import os
 import pandas as pd
 
 def get_values(values):
     user_values=pd.DataFrame([values])
     print(user_values)
     y=abc1.classifier.predict(user_values)
+    print(f'Adaboost classifier accuracy is {abc1.accuracy}')
     print(y)
